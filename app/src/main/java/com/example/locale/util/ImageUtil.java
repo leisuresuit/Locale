d003bec1c933ebd85b271c388589db0dc3930057
Larry Wang <larwang@paypal.com>
2015-10-30 00:33:52 -0700

    Use larger flag icons

--- app/src/main/java/com/example/locale/util/ImageUtil.java
+++ app/src/main/java/com/example/locale/util/ImageUtil.java
@@ -15,8 +15,8 @@ public class ImageUtil {
 
     public static @DrawableRes int getFlagIconId(Context context, String countryCode) {
         Resources resources = context.getResources();
-        int id = resources.getIdentifier("flag_" + countryCode.toLowerCase(), "drawable", context.getPackageName());
-        return (id > 0) ? id : R.drawable.flag_unknown;
+        int id = resources.getIdentifier(countryCode.toLowerCase() + "_flag", "drawable", context.getPackageName());
+        return (id > 0) ? id : R.mipmap.ic_launcher;
     }
 
     public static Drawable getFlagIcon(Context context, String countryCode) {
